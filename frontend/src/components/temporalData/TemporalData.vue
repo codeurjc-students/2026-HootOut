74166ade2ad17dddc4a8490b69f94dd3c68f807a
ivan <ivanchi88@gmail.com>
2026-09-19 00:33:27 +0200

    Websockets server receives a message and sends it back to front

--- frontend/src/components/temporalData/TemporalData.vue
+++ frontend/src/components/temporalData/TemporalData.vue
@@ -2,11 +2,42 @@
 import { onMounted, ref } from 'vue';
 import userService from '../../services/users/UserService'
 
-let users = ref([])
+let users = ref([]);
+let wsStatus = ref('');
+let wsMessage = ref('');
+let message = ref('');
 
 onMounted(async () => {
     users.value = [...await userService.getAllUsers()];
+    websocketsService.connect();
 });
+
+const websocketsService = {
+    socket: WebSocket,
+    connect: function () {
+        this.socket = new WebSocket(`${import.meta.env.VITE_WSS_URL}`);
+        this.socket.onopen = (event) => {
+            wsStatus.value = 'Connection Opened';
+        }
+        this.socket.onclose = (event) => {
+            wsStatus.value = 'Connection Closed';
+        }
+        this.socket.onerror = (event) => {
+            wsStatus.value = 'Connection Error';
+        }
+        this.socket.onmessage = (event) => {
+            wsMessage.value = event.data;
+        }
+    },
+    sendMessage: function (message) {
+        let data = message.value;
+        this.socket.send(data);
+    }
+}
+
+function sendMessage() {
+    websocketsService.sendMessage(message);
+}
 
 </script>
 
@@ -22,6 +53,16 @@ onMounted(async () => {
         </section>
         <section id='websockets'>
             <h2>Websockets:</h2>
+            <div> {{ wsStatus }}</div>
+            <div>
+                <p>Send Message:</p>
+                <input v-model="message" type="text" />
+                <button type="button" @click="sendMessage">Send Message</button>
+            </div>
+            <div>
+                <p>Received message:</p>
+                <div> {{ wsMessage }}</div>
+            </div>
         </section>
     </div>
 
